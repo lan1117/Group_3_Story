b4526acdc0f3972ca8634853f3eb73e3d0416065
julerrie <julerrie@bu.edu>
2016-10-09 20:53:27 -0400

    IOS application of challenge 3
    IOS application of challenge 3

---
+++ challenge3/iosapplication/challenge3Tests/challenge3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  challenge3Tests.swift
+//  challenge3Tests
+//
+//  Created by julerrie on 16/10/8.
+//  Copyright © 2016年 julerrie. All rights reserved.
+//
+
+import XCTest
+@testable import challenge3
+
+class challenge3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

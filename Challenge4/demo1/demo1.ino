383148758dab661639885d9355ba62c3c2ae051f
hyxmax5 <hyxmax5@bu.edu>
2016-10-19 20:06:39 -0400

    particle
    1

--- Challenge4/demo1/demo1.ino
+++ Challenge4/demo1/demo1.ino
@@ -4,9 +4,9 @@ const int PULLUP_RES = 9100;
 
 const double BETA = 3950;
 
-const double THERMISTOR_RES = 10000;
+const double THERMISTOR_RES = 9000;
 
-const double THERMISTOR_NOM_TEMP = 20;
+const double THERMISTOR_NOM_TEMP = 18;
 
 void setup()
 {
@@ -15,50 +15,28 @@ void setup()
 
 void loop()
 {
-    thermister_temp(analogRead(3));
-    delay(1000);
+    thermister_temp(analogRead(2));
+    delay(10000);
 }
 
 void thermister_temp(int aval)
 {
     double R, T;
     
-    R = (double) PULLUP_RES / ( 4950 /( (double) aval )   - 1 ) ;
+    R = (double) PULLUP_RES / ( ( (4950) / (double) aval ) - 1 );
     
     T = 1 / ( ( 1 / (THERMISTOR_NOM_TEMP + 273.15)) + ( ( 1/ BETA) * log(R / THERMISTOR_RES) ) );
     
     T -= 273.15;
-    //Particle.publish("analog", String(aval) + " *C", PRIVATE);
-   
-    Particle.publish("Temp_P3", String(T) + " *C", PRIVATE);
+    
+    //Spark.publish("Temperature", String(T) + " *C");
+    Particle.publish("Temp_P4", String(T) + " *C", PRIVATE);
+    Particle.publish("Temp", "4" + String(T) , PRIVATE);
 }
 
 void myHandler(const char *event, const char *data) {
   // Handle the integration response
 
 }
-      
-
-
-
-/*void loop() {
-  // Get some data
-  String data = String(10);
-  // Trigger the integration
-  Particle.publish("Temp", data, PRIVATE);
-  // Wait 60 seconds
-  delay(60000);
-}
-      
-          
-
-void setup() {
-  // Subscribe to the integration response event
-  Particle.subscribe("hook-response/Temp", myHandler, MY_DEVICES);
-}
-
-void myHandler(const char *event, const char *data) {
-  // Handle the integration response
-}*/
-          
+       
 

16e94e82268a62f0615a3551360e977305c9567e
hyxmax5 <hyxmax5@bu.edu>
2016-10-19 21:02:53 -0400

    Update demo1.ino

--- Challenge4/demo1.ino
+++ Challenge4/demo1.ino
@@ -4,9 +4,9 @@ const int PULLUP_RES = 9100;
 
 const double BETA = 3950;
 
-const double THERMISTOR_RES = 90000;
+const double THERMISTOR_RES = 100000;
 
-const double THERMISTOR_NOM_TEMP = 20;
+const double THERMISTOR_NOM_TEMP = 25;
 
 void setup()
 {
@@ -31,7 +31,6 @@ void thermister_temp(int aval)
     
     //Spark.publish("Temperature", String(T) + " *C");
     Particle.publish("Temp_P3", String(T) + " *C", PRIVATE);
-    Particle.publish("Temp", "3" + String(T), PRIVATE);
 }
 
 void myHandler(const char *event, const char *data) {
